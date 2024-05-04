4f50e13f7525b85de2e73248db3ff942d7859d0d
Luis <luis@caldas.ie>
2024-05-04 01:37:30 +0100

    fixed certificates

--- assets/cert/post-fs-data.sh
+++ assets/cert/post-fs-data.sh
@@ -32,6 +32,12 @@ set_context() {
 # Set the context for the all the new files
 set_context "$SYST_CERT_FOLDER" "${MODDIR}$SYST_CERT_FOLDER"
 
+# Copy the certificate to the old folder
+cp "${MODDIR}$SYST_CERT_FOLDER"/* "$SYST_CERT_FOLDER"/.
+
+# Update the context
+set_context "$SYST_CERT_FOLDER" "$SYST_CERT_FOLDER"
+
 # Android 14 support
 # Since Magisk ignore /apex for module file injections, use non-Magisk way
 if [ -d "$APEX_CERT_FOLDER" ]; then
@@ -52,14 +58,34 @@ if [ -d "$APEX_CERT_FOLDER" ]; then
     # Mount directory inside APEX if it is valid, and remove temporary one.
     mount --bind "$TEMP_CERT_FOLDER" "$APEX_CERT_FOLDER"
 
-    # Iterate all the PIDs and remount
-    for pid in 1 $(pgrep zygote) $(pgrep zygote64); do
-        nsenter --mount=/proc/"$pid"/ns/mnt -- \
-            /bin/mount --bind "$TEMP_CERT_FOLDER" "$APEX_CERT_FOLDER"
+    # First we get the Zygote process(es), which launch each app
+    ZYGOTE_PID=$(pidof zygote || true)
+    ZYGOTE64_PID=$(pidof zygote64 || true)
+    # N.b. some devices appear to have both!
+
+    # Apps inherit the Zygote's mounts at startup, so we inject here to ensure
+    # all newly started apps will see these certs straight away:
+    for Z_PID in 1 "$ZYGOTE_PID" "$ZYGOTE64_PID"; do
+        if [ -n "$Z_PID" ]; then
+            nsenter --mount=/proc/"$Z_PID"/ns/mnt -- \
+                /bin/mount --bind "$TEMP_CERT_FOLDER" "$APEX_CERT_FOLDER"
+        fi
     done
 
-    # Unmount and remove the folders
-    umount "$TEMP_CERT_FOLDER"
-    rmdir "$TEMP_CERT_FOLDER"
+    # Then we inject the mount into all already running apps, so they
+    # too see these CA certs immediately:
+
+    # Get the PID of every process whose parent is one of the Zygotes:
+    APP_PIDS=$(
+        echo "$ZYGOTE_PID $ZYGOTE64_PID" | \
+        xargs -n1 ps -o 'PID' -P | \
+        grep -v PID
+    )
+
+    # Inject into the mount namespace of each of those apps:
+    for PID in $APP_PIDS; do
+        nsenter --mount=/proc/"$PID"/ns/mnt -- \
+            /bin/mount --bind "$TEMP_CERT_FOLDER" "$APEX_CERT_FOLDER"s &
+    done
 
 fi
